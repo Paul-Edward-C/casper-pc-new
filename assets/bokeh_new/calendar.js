ccc7b3b93c10f0272a9c8422758637806e8fd742
Paul-Edward-C <77036253+Paul-Edward-C@users.noreply.github.com>
2022-08-09 16:12:38 +0800

    up

--- assets/bokeh_new/calendar.js
+++ assets/bokeh_new/calendar.js
@@ -1,6 +1,5 @@
 (function() {
   const fn = function() {
-    
     (function(root) {
       function now() {
         return new Date();
@@ -13,15 +12,11 @@
         root._bokeh_is_loading = undefined;
       }
     
-      
-      
     
-      const element = document.getElementById("efb07870-50f8-4487-86d2-d20d799d44e1");
+    const element = document.getElementById("21b0a95e-649a-4746-ab56-f268a092ad6e");
         if (element == null) {
-          console.warn("Bokeh: autoload.js configured with elementid 'efb07870-50f8-4487-86d2-d20d799d44e1' but no matching script tag was found.")
+          console.warn("Bokeh: autoload.js configured with elementid '21b0a95e-649a-4746-ab56-f268a092ad6e' but no matching script tag was found.")
         }
-      
-    
       function run_callbacks() {
         try {
           root._bokeh_onload_callbacks.forEach(function(callback) {
@@ -92,27 +87,21 @@
         document.body.appendChild(element);
       }
     
-      
-      const js_urls = ["https://cdn.bokeh.org/bokeh/release/bokeh-2.4.2.min.js", "https://cdn.bokeh.org/bokeh/release/bokeh-gl-2.4.2.min.js", "https://cdn.bokeh.org/bokeh/release/bokeh-widgets-2.4.2.min.js", "https://cdn.bokeh.org/bokeh/release/bokeh-tables-2.4.2.min.js", "https://cdn.bokeh.org/bokeh/release/bokeh-mathjax-2.4.2.min.js"];
+      const js_urls = ["https://cdn.bokeh.org/bokeh/release/bokeh-2.4.3.min.js", "https://cdn.bokeh.org/bokeh/release/bokeh-gl-2.4.3.min.js", "https://cdn.bokeh.org/bokeh/release/bokeh-widgets-2.4.3.min.js", "https://cdn.bokeh.org/bokeh/release/bokeh-tables-2.4.3.min.js", "https://cdn.bokeh.org/bokeh/release/bokeh-mathjax-2.4.3.min.js"];
       const css_urls = [];
-      
     
-      const inline_js = [
-        function(Bokeh) {
+      const inline_js = [    function(Bokeh) {
           Bokeh.set_log_level("info");
         },
-        
         function(Bokeh) {
           (function() {
             const fn = function() {
               Bokeh.safely(function() {
                 (function(root) {
                   function embed_document(root) {
-                    
-                  const docs_json = '{"13103c91-37be-4d6c-aa24-58eaed204914":{"defs":[],"roots":{"references":[{"attributes":{"data":{"Date":["Sat July 30","","Sun July 31","","Mon August  1","","","Tue August  2","","","","Thu August  4","","Fri August  5","","","","","","","","",""],"Release":["CN, Culture industries, quarterly","","CN, PMI","","KR, Full-month foreign trade, headlines","TW, NDC PMI","","JP, BOJ Accounts","JP, Monetary Base","KR, CPI","","JP, International Transactions in Securities, weekly","","CN, Balance of payments, preliminary","JP, Business conditions index, preliminary","JP, Consumption Activity Index","JP, Household survey","JP, Monetary Base and the BOJ&#x27;s Transactions","JP, Trade, 20D, provisional","JP, Wages, Provisional","KR, BOP , preliminary","TW, CPI","TW, International reserves"],"index":[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22]},"selected":{"id":"1015"},"selection_policy":{"id":"1014"}},"id":"1003","type":"ColumnDataSource"},{"attributes":{},"id":"1012","type":"StringEditor"},{"attributes":{"editor":{"id":"1012"},"field":"Date","formatter":{"id":"1011"},"title":"Date","width":150},"id":"1005","type":"TableColumn"},{"attributes":{"template":"\\n    &lt;style&gt;\\n        .slick-headerRowHeight {height:120px} \\n        .slick-column-name {font-family: Georgia; font-weight: 900; font-size: x-large}\\n        .slick-cell {font-family: Georgia; font-size: large}\\n    &lt;/style&gt;\\n    &lt;%= value %&gt;"},"id":"1004","type":"HTMLTemplateFormatter"},{"attributes":{},"id":"1011","type":"StringFormatter"},{"attributes":{"editor":{"id":"1013"},"field":"Release","formatter":{"id":"1004"},"title":"Release","width":350},"id":"1006","type":"TableColumn"},{"attributes":{},"id":"1015","type":"Selection"},{"attributes":{},"id":"1013","type":"StringEditor"},{"attributes":{},"id":"1014","type":"UnionRenderers"},{"attributes":{"columns":[{"id":"1005"},{"id":"1006"}],"index_position":null,"source":{"id":"1003"},"view":{"id":"1010"}},"id":"1008","type":"DataTable"},{"attributes":{"source":{"id":"1003"}},"id":"1010","type":"CDSView"}],"root_ids":["1008"]},"title":"Bokeh Application","version":"2.4.2"}}';
-                  const render_items = [{"docid":"13103c91-37be-4d6c-aa24-58eaed204914","root_ids":["1008"],"roots":{"1008":"efb07870-50f8-4487-86d2-d20d799d44e1"}}];
+                  const docs_json = '{"26efe585-538d-40ff-b50c-1ff347ea20bf":{"defs":[],"roots":{"references":[{"attributes":{},"id":"1015","type":"Selection"},{"attributes":{"template":"\\n    &lt;style&gt;\\n        .slick-headerRowHeight {height:120px} \\n        .slick-column-name {font-family: Georgia; font-weight: 900; font-size: x-large}\\n        .slick-cell {font-family: Georgia; font-size: large}\\n    &lt;/style&gt;\\n    &lt;%= value %&gt;"},"id":"1004","type":"HTMLTemplateFormatter"},{"attributes":{"columns":[{"id":"1005"},{"id":"1006"}],"index_position":null,"source":{"id":"1003"},"view":{"id":"1010"}},"id":"1008","type":"DataTable"},{"attributes":{"data":{"Date":["Sun August  7","","Mon August  8","","","","","Tue August  9","","Wed August 10","","","","","","Thu August 11","","Fri August 12",""],"Release":["CN, Merchandise trade, preliminary","","JP, Balance of Payments, June 2022, preliminary,Calendar Year January - June 2022C.Y., preliminary, 1st quarter 2022C.Y.","JP, Economy Watchers","JP, International Transactions in Securities, monthly","TW, Merchandise trade","","JP, Money Stock","","CN, CPI &amp; PPI, monthly","CN, Monetary supply and credit (earliest date in 5-day window)","JP, Corporate Goods Price Index","KR, Labour market","TW, Wages","","KR, Monetary and Liquidity Aggregates","","JP, International Transactions in Securities, weekly","KR, Export/Import Price Index"],"index":[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18]},"selected":{"id":"1015"},"selection_policy":{"id":"1014"}},"id":"1003","type":"ColumnDataSource"},{"attributes":{},"id":"1012","type":"StringEditor"},{"attributes":{},"id":"1013","type":"StringEditor"},{"attributes":{"editor":{"id":"1013"},"field":"Release","formatter":{"id":"1004"},"title":"Release","width":350},"id":"1006","type":"TableColumn"},{"attributes":{},"id":"1011","type":"StringFormatter"},{"attributes":{"source":{"id":"1003"}},"id":"1010","type":"CDSView"},{"attributes":{},"id":"1014","type":"UnionRenderers"},{"attributes":{"editor":{"id":"1012"},"field":"Date","formatter":{"id":"1011"},"title":"Date","width":150},"id":"1005","type":"TableColumn"}],"root_ids":["1008"]},"title":"Bokeh Application","version":"2.4.3"}}';
+                  const render_items = [{"docid":"26efe585-538d-40ff-b50c-1ff347ea20bf","root_ids":["1008"],"roots":{"1008":"21b0a95e-649a-4746-ab56-f268a092ad6e"}}];
                   root.Bokeh.embed.embed_items(docs_json, render_items);
-                
                   }
                   if (root.Bokeh !== undefined) {
                     embed_document(root);
@@ -138,18 +127,14 @@
             else document.addEventListener("DOMContentLoaded", fn);
           })();
         },
-        function(Bokeh) {
-        
-        
+    function(Bokeh) {
         }
       ];
     
       function run_inline_js() {
-        
         for (let i = 0; i < inline_js.length; i++) {
           inline_js[i].call(root, root.Bokeh);
         }
-        
       }
     
       if (root._bokeh_is_loading === 0) {
