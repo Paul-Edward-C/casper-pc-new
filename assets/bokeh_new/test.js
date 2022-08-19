35fb84833763a7f7ba5911e2c7a6ca577de358de
Paul-Edward-C <77036253+Paul-Edward-C@users.noreply.github.com>
2022-08-19 08:03:17 +0800

    Update test.js

--- assets/bokeh_new/test.js
+++ assets/bokeh_new/test.js
@@ -1,4 +1,4 @@
-    <script type="text/javascript">
+
         (function() {
   const fn = function() {
     Bokeh.safely(function() {
@@ -31,4 +31,3 @@
   if (document.readyState != "loading") fn();
   else document.addEventListener("DOMContentLoaded", fn);
 })();
-    </script>
