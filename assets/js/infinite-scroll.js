cfa2dd5a009d4c2b5308274edca8f0ff7cafebff
Paul-Edward-C <77036253+Paul-Edward-C@users.noreply.github.com>
2023-09-03 19:33:16 +0100

    up

--- assets/js/infinite-scroll.js
+++ assets/js/infinite-scroll.js
@@ -14,8 +14,6 @@
  */
 
 (function (window, document) {
-    if (document.documentElement.classList.contains('no-infinite-scroll')) return;
-
     // next link element
     var nextElement = document.querySelector('link[rel=next]');
     if (!nextElement) {
